c3b5e612d4003b0fb43a4a8e2977e903e1e0a4ca
jito <jitokj3@gmail.com>
2020-11-16 19:14:44 +0530

    connected with alan-ai

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 
 const alanKey = "d925c2476823b46672d4218af4223fbc2e956eca572e1d8b807a3e2338fdd0dc/stage";
+  
 const App = () => {
     
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand: ({command})=>{
-                if(command === 'testCommand'){
-                    alert('This code was executed');
+            onCommand: ({command,saved_articles})=>{
+                if(command === 'newHeadlines'){
+                    console.log(saved_articles);
                 }
             }
         })
