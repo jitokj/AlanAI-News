60c1060a51a019b6cfb5620a89bc279780a1d168
jito <jitokj3@gmail.com>
2020-11-16 19:27:01 +0530

    added newsCard component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { useState,useEffect } from 'react';
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+import NewsCards from "./components/NewsCards/NewsCards";
+
 
 const alanKey = "d925c2476823b46672d4218af4223fbc2e956eca572e1d8b807a3e2338fdd0dc/stage";
   
 const App = () => {
+
+    const [newsArticles,setNewsArticles]=useState([]);
     
     useEffect(()=>{
         alanBtn({
             key: alanKey,
             onCommand: ({command,saved_articles})=>{
                 if(command === 'newHeadlines'){
-                    console.log(saved_articles);
+                    setNewsArticles(saved_articles);
                 }
             }
         })
     },[])
     return (
         <div>
-            <h1>Alan-ai </h1>
+           <NewsCards articles = {newsArticles} />
         </div>
     );
 };
