5d0db3341055db735394e201293cecbe0baeae44
jito <jitokj3@gmail.com>
2020-11-16 22:24:15 +0530

    added styles in components

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Card,CardActions,CardActionArea,CardContent,CardMedia,Button,Typography} from "@material-ui/core";
+import img  from "../../images/news.jpg";
+import useStyles from "./styles";
 
-const NewsCard = () => {
+const NewsCard = ({article: { description,publishedAt,source,title,url,urlToImage },i}) => {
+    const classes = useStyles()
     return (
-        <div>
-            News Card
-        </div>
+      
+      <Card>
+          <CardActionArea>
+              <CardMedia className={classes.media} image={urlToImage || img}/>
+              <div>
+                  <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                  <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+              </div>
+                    <Typography gutterBottom variant="h5">{title}</Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                    </CardContent>
+             
+          </CardActionArea>
+          <CardActions>
+              <Button size="small" color="primary">Learn More</Button>
+              <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+          </CardActions>
+      </Card>
     );
 };
 
