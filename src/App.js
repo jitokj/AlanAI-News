18bcfa88e10ca830242e53bbb870b9702fbd0457
jito <jitokj3@gmail.com>
2020-11-17 15:58:38 +0530

    added new search terms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
 
 
 const alanKey = "d925c2476823b46672d4218af4223fbc2e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -9,19 +10,23 @@ const alanKey = "d925c2476823b46672d4218af4223fbc2e956eca572e1d8b807a3e2338fdd0d
 const App = () => {
 
     const [newsArticles,setNewsArticles]=useState([]);
+    const classes = useStyles();
     
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand: ({command,saved_articles})=>{
+            onCommand: ({command,articles})=>{
                 if(command === 'newHeadlines'){
-                    setNewsArticles(saved_articles);
+                    setNewsArticles(articles);
                 }
             }
         })
     },[])
     return (
         <div>
+        <div className={classes.logoContainer}>
+            <img src="https://alan.app/voice/images/previews/preview.jpg" alt="alan Logo" className={classes.alanLogo} />
+        </div>
            <NewsCards articles = {newsArticles} />
         </div>
     );
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -16,7 +16,22 @@ const NewsCards = ({articles}) => {
         return (
             <Grow in>
         <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            test
+            {infoCards.map(infoCard=>(
+                <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+                    <div className={classes.card} style={{backgroundColor: infoCard.color}}>
+                        <Typography variant="h5">{infoCard.title}</Typography>
+                        {infoCard.info ?
+                         (<Typography variant="h6">
+                         <strong>{infoCard.title.split(" ")[2]}:
+                         </strong>
+                         <br />
+                         {infoCard.info}
+                       
+                         </Typography>): null}
+                         <Typography variant="h6">Try Saying: <br /><i>{infoCard.text}</i></Typography>
+                    </div>
+                </Grid>
+            ))}
         </Grid>
        </Grow>
         )
